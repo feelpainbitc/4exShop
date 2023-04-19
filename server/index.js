b53ade0ed93da66ce57423851b9e4301156366b2
Dima Cheshko <dima4ka.cheshko@gmail.com>
2023-04-19 21:04:14 +0300

    added models for database

--- server/index.js
+++ server/index.js
@@ -1,9 +1,23 @@
 const express = require('express')
 const { callbackify } = require('util')
 require('dotenv').config()
+const sequelize=require('./db')
+const models = require('./models/models')
 
 const PORT=process.env.PORT || 5000
 
 const app =express()
 
-app.listen(PORT, () => console.log(`Server started on port ${5000}`))
+
+const start = async () =>{
+    try{
+        await sequelize.authenticate()
+        await sequelize.sync()
+        app.listen(PORT, () => console.log(`Server started on port ${5000}`))
+    }
+    catch(e){
+        console.log(e)
+    }
+}
+
+start()
