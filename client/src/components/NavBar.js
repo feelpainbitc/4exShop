c94bb6904352f1736399296abd0af8fd1b90e45e
Dima Cheshko <dima4ka.cheshko@gmail.com>
2023-04-30 17:26:03 +0300

    navbar added

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -11,19 +11,12 @@ export const NavBar = (props) => {
     const {user}=useContext(Context)
   return(
     <div className='navbar'>
-        <div className='navbar_left'>
-            <button className='btns'>Для мужчин</button>
-            <button className='btns'>Для женщин</button>
-            <button className='btns'>Унисекс</button>
+        <div className='logo'>4EX SHOP</div>
+        <div className='btns'>
+            <button className='navbar_adm'>Админ панель</button>
+            <button className='navbar_ent'>Войти</button>
         </div>
-        <div className='logo'>
-            4EX SHOP
-        </div>
-        <div className='navbar_right'>
-            <div className='basket'>Basket</div>
-            <div className='profile'>Profile</div>
-        </div>
-    </div>  
+    </div>
    )
 
  }
