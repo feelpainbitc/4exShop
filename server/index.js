c5e2651002ac2259cab04e846c8a00b419704543
Dima Cheshko <dima4ka.cheshko@gmail.com>
2023-05-04 19:21:03 +0300

    database added routes added

--- server/index.js
+++ server/index.js
@@ -3,10 +3,15 @@ const { callbackify } = require('util')
 require('dotenv').config()
 const sequelize=require('./db')
 const models = require('./models/models')
+const cors = require('cors')
+const router=require('./routes/index')
 
 const PORT=process.env.PORT || 5000
 
 const app =express()
+app.use(cors())
+app.use(express.json()) 
+app.use('/api',router)
 
 
 const start = async () =>{
---
+++ server/routes/index.js
@@ -0,0 +1,20 @@
+const Router = require('express')
+const router = new Router()
+const deviceRouter= require('./deviceRouter')
+const typeRouter= require('./typeRouter')
+const brandRouter= require('./brandRouter')
+const userRouter= require('./userRouter')
+const basketRouter= require('./basketRouter')
+
+
+
+
+router.use('/user',userRouter)
+router.use('/type',typeRouter)
+router.use('/brand',brandRouter)
+router.use('/device',deviceRouter)
+
+
+
+
+module.exports = router
