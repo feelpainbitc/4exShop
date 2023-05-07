c9c1ff86729fdded1bf28e8014ede70a9bcf7691
Dima Cheshko <dima4ka.cheshko@gmail.com>
2023-05-07 13:24:36 +0300

    controllers +bd

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,6 @@
 const { query } = require("express")
+const ApiError=require('../error/ApiError')
+
 
 class UserController{
     async registration(req,res){
@@ -7,9 +9,12 @@ class UserController{
     async login(req,res){
         
     }
-    async check(req,res){
+    async check(req,res,next){
         const {id} =req.query
-        res.json(query.id)
+        if(!id){
+            return next(ApiError.badRequest('не задан ID'))
+        }
+        res.json(id);
     }
 }
 module.exports=new UserController()
