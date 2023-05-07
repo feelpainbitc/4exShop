fd6e589a55485a732163627b4bca9b2f1590ecf2
Dima Cheshko <dima4ka.cheshko@gmail.com>
2023-05-07 15:04:09 +0300

    authmw added

--- server/controllers/deviceController.js
+++ server/controllers/deviceController.js
@@ -11,8 +11,20 @@ class DeviceController{
         const {img} =req.files
         let fileName=uuid.v4()+".jpg"
         img.mv(path.resolve(__dirname,'../','static',fileName))
-
         const device= await Device.create({name,price,brandId,typeId,img:fileName})
+
+        if(info){
+            info=JSON.parse(info)
+            info.forEach(i=>
+                DeviceInfo.create({
+                    title:i.title,
+                    description:i.description,
+                    deviceId:device.id
+                })
+            )
+        }
+
+
         return res.json(device)
        }
        catch(e){
@@ -42,7 +54,14 @@ class DeviceController{
     }
 
     async getOne (req,res){
-        
+        const {id} =req.params
+        const device=await Device.findOne(
+            {
+                where:{id},
+                include:[{model:DeviceInfo,as:'info'}]
+            },
+        )
+        return res.json(device)
     }
 
 }
